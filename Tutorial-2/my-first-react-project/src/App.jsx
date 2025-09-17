1fff231e47afdf07c0675e4408840355b1842688
zewais <arch.ziad.ewais@gmail.com>
2025-09-17 10:42:56 -0400

    final diamond world

--- Tutorial-2/my-first-react-project/src/App.jsx
+++ Tutorial-2/my-first-react-project/src/App.jsx
@@ -4,18 +4,24 @@ import Card from "./Components/Card";
 
 function App() {
   return (
-    <>
+    <div className="main-div">
       <h1>Diamond World</h1>
       <div className="main-container">
         <Card image="src\assets\images\1.jpg" name="princess" price="$1,350" />
-        <Card image="src\assets\images\2.jpg" name="swan" price="$900" />
+        <Card
+          image="src\assets\images\2.jpg"
+          name="swan"
+          price="$900"
+          sale={true}
+        />
         <Card
           image="src\assets\images\3.jpg"
           name="collection"
           price="$1,100"
+          sale={true}
         />
       </div>
-    </>
+    </div>
   );
 }
 
--- Tutorial-2/my-first-react-project/src/Components/Card.jsx
+++ Tutorial-2/my-first-react-project/src/Components/Card.jsx
@@ -1,10 +1,19 @@
 export default function Card(props) {
   console.log(props);
   return (
-    <div className="Card-Component">
+    <div
+      className={
+        props.sale ? "Card-Component Card-Sale-Glow" : "Card-Component"
+      }
+    >
       <img src={props.image} alt="" width="100px" />
       <h2>{props.name}</h2>
+      {props.sale ? <h3 className="Card-Sale">Sale</h3> : <h3>&nbsp;</h3>}
       <p>{props.price}</p>
+      <button>Buy Now</button>
     </div>
   );
 }
+
+//prop.sale && "message" => if prop.sale === true, message will appear
+//prop.sale || "message" => if prop.sale === false, message will appear
